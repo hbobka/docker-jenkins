8b8aaa491d4e87f393fa408fd802671db181914d
Henry Bobka <henry.bobka@t-systems.com>
2021-09-23 21:18:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let hello = "Hello World!";
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h3>${hello}</h3>
+        </section>        
+      </body>
+    </html>
+  `);
+});
+
+app.listen(80);
